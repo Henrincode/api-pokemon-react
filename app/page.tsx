5a281df1e7d70731566c65ebd99d6f3b6889ee2a
Henrincode <spellctro@gmail.com>
2026-03-11 02:48:43 -0300

    refactor: refatoring list and profile

--- app/page.tsx
+++ app/page.tsx
@@ -7,16 +7,22 @@ export default async function Home() {
   const pokemons = data.results
 
   return (
-    <div className="flex min-h-screen flex-wrap gap-4 items-center justify-center bg-zinc-50 font-sans dark:bg-black p-10">
+    <>
+      <img src="https://pngimg.com/uploads/pokemon_logo/small/pokemon_logo_PNG5.png" alt="" className="mx-auto"/>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7 gap-4 p-4">
 
-      {pokemons.map((p: any) => {
-        const id = p.url.split('/').filter(Boolean).pop()
-        return (
-          <Link href={`/pokemon/${p.name}`} key={p.name} className="text-zinc-900 dark:text-zinc-100 border p-2 rounded">
-            {p.name}
-          </Link>
-        )
-      })}
-    </div>
+        {pokemons.map((p: any) => {
+          const id = p.url.split('/').filter(Boolean).pop()
+
+          if (id >= 10000) return
+          return (
+            <Link href={`/pokemon/${p.name}`} key={p.name} className="p-2 border rounded-lg text-center border-blue-400/50 bg-blue-50 hover:bg-blue-300 transition-all hover:-translate-y-2">
+              <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`} alt="" className="w-full"/>
+              {p.name}
+            </Link>
+          )
+        })}
+      </div>
+    </>
   );
 }
--- app/pokemon/[name]/page.tsx
+++ app/pokemon/[name]/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import { MdNavigateBefore, MdNavigateNext } from "react-icons/md"
 
 interface Props {
     params: Promise<{ name: string }>
@@ -11,63 +12,72 @@ export default async function Pokemon({ params }: Props) {
 
     const resPokelist = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10000")
     const data = await resPokelist.json()
-    const pokemons = data.results
+
+    const pokemons = data.results.filter((p: any) => {
+        const id = parseInt(p.url.split('/').filter(Boolean).pop())
+        return id < 10000
+    })
 
     const atual = pokemons.findIndex((p: any) => p.name === name)
 
     return (
-        <div className="
+        <div className="h-dvh flex flex-col justify-center items-center p-4">
+            <img src="https://pngimg.com/uploads/pokemon_logo/small/pokemon_logo_PNG5.png" alt="" />
+            <div className="
             flex flex-col justify-center items-center gap-
-            w-lg p-4 mx-auto mt-10 border border-blue-400/50 rounded-2xl
+            max-w-lg w-full p-4 mx-auto border border-blue-400/50 rounded-2xl
             bg-blue-50
         ">
-            <div className="
-            flex flex-row items-center w-full
+                <div className="
+            flex flex-row justify-center items-center w-full
             ">
-                <div className={`${atual || 'hidden'}`}>
-                    <Link href={`/pokemon/${pokemons[atual - 1]?.name}`}>{'<'}</Link>
-                </div>
-                <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`} alt=""
-                    className="
-                    flex-1
-                    size-50
+                    <div className={`${atual || 'hidden'}`}>
+                        <Link href={`/pokemon/${pokemons[atual - 1]?.name}`} className="flex rounded-full bg-blue-400 hover:animate-pulse"><MdNavigateBefore size={30} /></Link>
+                    </div>
+                    <div className="flex-1 flex flex-row justify-center">
+                        <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`} alt=""
+                            className="
+                    w-80
                 "/>
-                <div className={`${pokemons.length === atual && 'hidden'}`}>
-                    <Link href={`/pokemon/${pokemons[atual + 1].name}`}>{'>'}</Link>
+                    </div>
+                    <div className={`${pokemons.length - 1 == atual && 'hidden'}`}>
+                        <Link href={`/pokemon/${pokemons[atual + 1]?.name}`} className="flex rounded-full bg-blue-400 hover:animate-pulse"><MdNavigateNext size={30} /></Link>
+                    </div>
+                </div>
+                <p>id: {pokemon.id}</p>
+                <p>nome: {pokemon.name}</p>
+                <div className="flex flex-col items-center">
+                    <div>Tipos:</div>
+                    <ul className="flex flex-row justify-center flex-wrap gap-1">
+                        {pokemon.types.map(({ type }: any) => (
+                            <li key={type.name} className="px-2 rounded-full text-sm bg-blue-300">
+                                {type.name}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="flex flex-col items-center">
+                    <div>Abilidades:</div>
+                    <ul className="flex flex-row justify-center flex-wrap gap-1">
+                        {pokemon.abilities.map(({ ability }: any) => (
+                            <li key={ability.name} className="px-2 rounded-full text-sm bg-blue-300">
+                                {ability.name}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="flex flex-col items-center">
+                    <div>Jogos:</div>
+                    <ul className="flex flex-row justify-center flex-wrap gap-1">
+                        {pokemon.game_indices.map(({ version }: any) => (
+                            <li key={version.name} className="px-2 rounded-full text-sm bg-blue-300">
+                                {version.name}
+                            </li>
+                        ))}
+                    </ul>
                 </div>
             </div>
-            <p>id: {pokemon.id}</p>
-            <p>nome: {pokemon.name}</p>
-            <div className="flex flex-col items-center">
-                <div>Tipos:</div>
-                <ul className="flex flex-row justify-center flex-wrap gap-1">
-                    {pokemon.types.map(({ type }: any) => (
-                        <li key={type.name} className="px-2 rounded-full text-sm bg-blue-300">
-                            {type.name}
-                        </li>
-                    ))}
-                </ul>
-            </div>
-            <div className="flex flex-col items-center">
-                <div>Abilidades:</div>
-                <ul className="flex flex-row justify-center flex-wrap gap-1">
-                    {pokemon.abilities.map(({ ability }: any) => (
-                        <li key={ability.name} className="px-2 rounded-full text-sm bg-blue-300">
-                            {ability.name}
-                        </li>
-                    ))}
-                </ul>
-            </div>
-            <div className="flex flex-col items-center">
-                <div>Jogos:</div>
-                <ul className="flex flex-row justify-center flex-wrap gap-1">
-                    {pokemon.game_indices.map(({ version }: any) => (
-                        <li key={version.name} className="px-2 rounded-full text-sm bg-blue-300">
-                            {version.name}
-                        </li>
-                    ))}
-                </ul>
-            </div>
+            <Link href={'/'} className="px-6 py-2 mt-2 rounded-full font-semibold text-xl text-white bg-blue-500 hover:bg-blue-700 transition-all">Voltar</Link>
         </div>
     )
 }
